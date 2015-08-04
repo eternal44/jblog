4b59d79802fd42171698cf09c562e02f9fa7fb1d
James Youn <jamesyoun710@gmail.com>
2015-08-04 15:06:54 -0700

    Refactor comments_controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,15 +2,23 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @comment = @commentable.comments.new comment_params
-    @comment.user = current_user
-    @comment.save
-    redirect_to @commentable, notice: "Your comment was successfully posted!"
+    build_comment
+    save_comment or render 'new'
   end
 
   private
 
     def comment_params
       params.require(:comment).permit(:body) # no need to set params for commentable since it's already set
+    end
+
+    def build_comment
+      @comment = @commentable.comments.build comment_params
+      @comment.user = current_user
+    end
+
+    def save_comment
+      @comment.save
+      redirect_to @commentable
     end
 end
