6b8105737bbcba6a2884a8d2ead6fb2ce7cd8fc1
James Youn <jamesyoun710@gmail.com>
2015-08-26 15:30:55 -0700

    Clean white spaces

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -3,17 +3,18 @@ class PostPolicy < ApplicationPolicy
   def update?
     user.present? && user.admin?
   end
-  
+
   # def show? # authorizing show action isn't working.
   #   true    # left it here because it doesn't matter for the blog anyway.
   # end
-  
+
   def create?
     user.present? && user.admin?
   end
 
+  # leave this hear for next round of features
   def destroy?
     user.present? && user.admin?
   end
-
 end
+
