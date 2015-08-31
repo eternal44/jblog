b34d66a97a2c4d27be1950d71955b5bb8a928313
James Youn <jamesyoun710@gmail.com>
2015-08-31 16:06:09 -0700

    Build views for dashboard
    Not DRY - refactor later

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
   get 'static_pages/about'
+  get 'dashboard/overview'
+  get 'dashboard/drafted_posts'
+  get 'dashboard/scheduled_posts'
 
   resources :posts do
     resources :comments, module: :posts, only: [:create]
