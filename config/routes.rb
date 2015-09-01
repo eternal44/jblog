20d71b25ee5d7e23ebc4a25d76e0b4cc321c0c27
James Youn <jamesyoun710@gmail.com>
2015-08-31 17:35:10 -0700

    Clean up routes & related paths
    Wasn't using 'about' route & 'contact' route was too long.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'static_pages/contact'
-  get 'static_pages/about'
+  get 'contact' => 'static_pages#contact'
   get 'dashboard/overview'
-  get 'dashboard/drafted_posts'
-  get 'dashboard/scheduled_posts'
 
   resources :posts do
     resources :comments, module: :posts, only: [:create]
