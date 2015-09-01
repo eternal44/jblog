a08e674451bfeb05bb7271ea7b53a825d06593f9
James Youn <jamesyoun710@gmail.com>
2015-08-31 18:39:43 -0700

    Manually include post model
    Same issue.  Not sure why git didn't merge automatically

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,23 @@
 class Post < ActiveRecord::Base
+  default_scope     ->{ order('created_at DESC') }
   scope :draft,     ->{ where(published_at: nil) }
   scope :published, ->{ where.not(published_at: nil).where("published_at <= ?", Time.zone.now) }
-  scope :scheduled, ->{ where.not(published_at: nil).where("published_at > ?", Time.zone.now)}
+  scope :scheduled, ->{ where.not(published_at: nil).where("published_at > ?", Time.zone.now) }
 
   attr_accessor :status
 
-  before_validation :clean_up_status
+  # mount_uploader :picture, PictureUploader
 
+  before_validation :clean_up_status
+  validates :title, presence: true
+  validates :text, presence: true
+
+  has_many :comments, as: :commentable
+
+  private
+  # watch screen cast and figure out the logic.  Has something to do with an easier way of
+  # working with javascript and generating html by enforcing the logic via rails instead...
+  # I think I got that right?
   def clean_up_status
     self.published_at = case status
                         when "Draft"
@@ -16,6 +27,6 @@ class Post < ActiveRecord::Base
                         else
                           published_at
                         end
-    true
+    true # for when drafts are saved so we don't get errors.
   end
 end
