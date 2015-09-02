5a9a45e5e9b42e25a368db1c82eed76ac54fd4d6
James Youn <jamesyoun710@gmail.com>
2015-09-02 02:00:25 -0700

    Rename test
    Forgot to add this before

--- test/integration/posts_test.rb
+++ test/integration/posts_test.rb
@@ -29,13 +29,13 @@ class PostsTest < ActionDispatch::IntegrationTest
     assert page.has_text?('jamesyoun710')
   end
 
-  test 'login' do
+  test 'login and create a post' do
     visit new_user_session_path
     assert page.has_field?('Email', type: 'email')
     fill_in('Email', with: 'jamesyoun710@gmail.com')
     fill_in('Password', with: 'gooneen44')
     click_on('Log in') # I should have been able to login with the above action # I should have been able to login with the above action
-    login_as(@admin)
+    # login_as(@admin)
     visit root_path
     assert page.has_link?('Logout')
     assert page.has_link?('New Post')
