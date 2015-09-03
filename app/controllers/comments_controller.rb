6998a1cc0e05235e8c47c272c4caefc68331688a
James Youn <jamesyoun710@gmail.com>
2015-09-03 00:07:31 -0700

    Refactor comments controller
    Prepping to add json

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,23 +2,16 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    build_comment
-    save_comment or render 'new'
+    @comment = @commentable.comments.build comment_params
+    @comment.user = current_user
+    @comment.save
+
+    redirect_to @commentable
   end
 
   private
 
-    def comment_params
-      params.require(:comment).permit(:body) # no need to set params for commentable since it's already set
-    end
-
-    def build_comment
-      @comment = @commentable.comments.build comment_params
-      @comment.user = current_user
-    end
-
-    def save_comment
-      @comment.save
-      redirect_to @commentable
-    end
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
