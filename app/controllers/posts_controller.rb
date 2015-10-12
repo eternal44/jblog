5c0c6e52d2cf5d5c62139992de1619f9c8c53529
James Youn <jamesyoun710@gmail.com>
2015-10-12 14:03:06 -0700

    Refactor post params & controller variables

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,46 +1,48 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @q = Post.published.ransack(params[:q])
     @posts = @q.result(distinct: true)
   end
 
   def show
-    @post = Post.find(params[:id])
     authorize @post
   end
 
   def new
-    @post ||= post_scope.build
-    @post.attributes = post_params
+    @post = Post.new
     authorize @post
   end
 
   def create
-    @post ||= post_scope.build
-    @post.attributes = post_params
+    @post = Post.new(post_params)
     authorize @post
     save_post or render 'new'
   end
 
   def edit
-    @post = Post.find(params[:id])
-    authorize @post
-    @post ||= post_scope.build
-    @post.attributes = post_params
     authorize @post
   end
 
   def update
-    @post = Post.find(params[:id])
-    authorize @post
-    @post ||= post_scope.build
-    @post.attributes = post_params
-    authorize @post
-    save_post or render 'edit'
+    # @post ||= post_scope.build
+    # @post.attributes = post_params
+    # authorize @post
+    # save_post or render 'edit'
+
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.'}
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-    @post = Post.find(params[:id])
     authorize @post
     @post.destroy
     redirect_to posts_path
@@ -55,14 +57,16 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    post_params = params[:post]
-    post_params ? post_params.permit(:title, :text, :published_at, :status) : {}
+    params.require(:post).permit(:title, :text, :published_at, :status)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
   def post_scope
     Post.all #fill in
   end
-
 end
 # class PostsController < ApplicationController
 #   before_action :set_post, only: [:show, :edit, :update, :destroy]
